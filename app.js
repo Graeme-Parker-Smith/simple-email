fe24f2cd48bcddf269a9c99fb014b0e9e971de71
Graeme <graemster@gmail.com>
2021-02-05 02:48:17 -0600

    us sendFile

--- app.js
+++ app.js
@@ -74,8 +74,8 @@ app.post('/api/sendemail', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-	// res.sendFile(path.join(__dirname + './fit/home.html'));
-	res.redirect('https://www.specializedfit.com');
+	res.sendFile(path.join(__dirname + './fit/home.html'));
+	// res.redirect('https://www.specializedfit.com');
 });
 
 const PORT = process.env.PORT || 3000;
